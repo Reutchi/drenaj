b50fcfb0bb81e8657dab7d2b02ec7a8694c99f1c
Developer <developer@MacBook-Pro-Dan.local>
2024-03-13 08:53:46 +0200

    styling carousel

--- components/WorkCarousel.js
+++ components/WorkCarousel.js
@@ -5,32 +5,56 @@ import Arrow from '../public/orangeArrow.svg'
 import Styles from './css/work-carousel.module.scss'
 
 import photoSlide from '../public/img1.jpg'
+import building from '../public/building.jpg'
+
+import {useState} from "react";
+
 const WorkCarousel = () => {
+
+    const [imageCarousel, setImageCarousel] = useState(0)
+
+
+    const photosSlide = [
+        {id:1,photo: photoSlide, alt: 'Picture of the author'},
+        {id:2,photo: building, alt: 'Picture of the author'},
+    ]
+
+    const handleSelectPrev = () => {
+        setImageCarousel(prevState => (prevState <= 0 ? prevState : prevState - 1 ))
+    }
+
+    const handleSelectNext = () => {
+        setImageCarousel(prevState => (prevState >= 2 ? prevState : prevState + 1))
+    }
+
+    const handleSelectDot = (idx) => {
+        idx !== imageCarousel && setImageCarousel(idx)
+    }
+
+
+
     return(
      <div className={Styles.carousel}>
-         <Image
-             width={1041}
-             height={500}
-             src={photoSlide}
-             style={{objectFit:"cover"}}
-             alt="Picture of the author"
-         />
+         <div className={Styles.imageWrapper}>
+             {photosSlide.map(({id,photo,alt}) => {
+                 return(
+                     <Image
+                         key={id}
+                         height={380}
+                         src={photo}
+                         style={{objectFit:"cover"}}
+                         alt={alt}
+                     />
+                 )
+             })}
+
+         </div>
          <ul className={Styles.slides}>
-             <li>
-                 <Image
-                     alt={'image'}
-                     src={Arrow}
-                 />
-             </li>
-             <li className={`${Styles.ellpse} ${Styles.selected}`}></li>
-             <li className={Styles.ellipse}></li>
-             <li className={Styles.ellipse}></li>
-             <li>
-                 <Image
-                     alt={'image'}
-                     src={Arrow}
-                 />
-             </li>
+             <li onClick={handleSelectPrev}><Image alt={'image'} src={Arrow}/></li>
+             {Array(3).fill(0).map((i,idx) => (
+                 <li onClick={() => handleSelectDot(idx)} className={`${imageCarousel === idx ? Styles.selected : Styles.ellipse}`}></li>
+             ))}
+             <li onClick={handleSelectNext}><Image alt={'image'} src={Arrow}/></li>
          </ul>
      </div>
     )
